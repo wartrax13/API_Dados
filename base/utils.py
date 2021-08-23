a6d64e866c53c12fbce8a7637d874bbc7f7d8ddc
wartrax13 <pedro.moisesandrade@gmail.com>
2021-08-23 14:58:14 -0300

    Incompleto, pronto para entrega

--- base/utils.py
+++ base/utils.py
@@ -30,32 +30,6 @@ def send_message(text, chat_id):
 
 
 # Testando um GET ----------
-'''def get_message(update_id):
-    url = 'https://api.telegram.org/bot{0}/getUpdates?offset={1}'.format(TOKEN, update_id)
-    response = requests.get(url)
-    response_json = response.json()
-    last_update_id = update_id
-    for i in response_json['result']:
-        first_name = i['message']['chat']['first_name']
-        last_name = i['message']['chat']['last_name']
-        chat_id = i['message']['chat']['id']
-        last_update_id = i['update_id']
-        text = i['message']['text']
-        print(first_name, last_name, chat_id, update_id, text)
-        if last_update_id != update_id:
-            if "Chocolate" in text:
-                send_message('Chocolate é do Mexico', chat_id)
-            elif "Pimenta" in text:
-                send_message('Habanero?', chat_id)
-    return last_update_id'''
-
-
-
-
-
-
-
-
 '''
 def get_message(update_id):
     url = 'https://api.telegram.org/bot{0}/getUpdates?offset={1}'.format(TOKEN, update_id)
